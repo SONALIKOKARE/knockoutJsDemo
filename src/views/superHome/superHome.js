885a5ecb63ab57c7ab4da68d4381d429dada78d7
sonali_cyb <sonaliko@cybage.com>
2014-06-06 17:47:53 +0530

    Added add and remove functionality for classmates

--- src/views/superHome/superHome.js
+++ src/views/superHome/superHome.js
@@ -3,7 +3,7 @@ define([
 	"mediator",
 	"less!../../../../../../../less/superHome"
 ],function(superHomeTemplate, mediator){
-	var self,viewModel;
+	var self;
 	return function(){
 		this.render = function() {
 			self = this;
@@ -12,9 +12,19 @@ define([
 		},
 		this._addEvent = function(){
 			function friend(name){
+				//console.log(name);
+				return {
+					name:ko.observable(name),
+				};
+			}
+			function classmate(name){
 				console.log(name);
 				return {
-					name:ko.observable(name)
+					name:ko.observable(name),
+					removeClassmate: function(){
+						console.log('dfsdf');
+						viewModel.classmates.remove(this);
+					}
 				};
 			}
 			viewModel = {
@@ -22,7 +32,14 @@ define([
 				lastText:ko.observable("Goddd"),
 				firstName:"sonali",
 				lastName:"kokare",
-				friends:ko.observableArray([new friend('amit'), new friend('sachin'), new friend('bunty')])
+				friends:ko.observableArray([new friend('amit'), new friend('sachin'), new friend('bunty')]),
+				addFriend: function(){
+					this.friends.push(new friend("yogesh"))
+				},
+				classmates:ko.observableArray([new classmate('shri'), new classmate('prasad'), new classmate('ashwin')]),
+				addClassmate: function(){
+					this.classmates.push(new classmate("mangesh"))
+				},
 			};
 			viewModel.jointText = ko.dependentObservable(function(){
 				console.log(viewModel)
